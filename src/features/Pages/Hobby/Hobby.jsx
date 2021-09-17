64bf489226e31a5ecff89d141b6e107fca4a915d
Ryan <ryan@ryannd.com>
2021-09-17 11:48:49 -0500

    Added ability to follow a hobby and rate it once it's followed

--- src/features/Pages/Hobby/Hobby.jsx
+++ src/features/Pages/Hobby/Hobby.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
@@ -10,20 +10,29 @@ export default function Hobby(props) {
     const allHobbies = useSelector((state) => state.hobbies.value.payload);
     const user = useSelector((state) => state.user.value.payload);
     const dispatch = useDispatch();
-    const [status, setStatus] = useState("");
+    let status = ""
+    const [userHobbyRating, setUserHobbyRating] = useState(-1);
+    let tempHobbyRating = 0;
     const history = useHistory();
     const hobbyId = props.match.params.id;
-    let currentHobby;
-    try {
-        currentHobby = allHobbies.filter((hobby) => hobby.hobbyId == hobbyId)[0];
-    } catch {
-        history.push("/notfound");
-    }
+    let [currentHobby, setCurrentHobby] = useState();
+    useEffect(() => {
+        try {
+            setCurrentHobby(allHobbies.filter((hobby) => hobby.hobbyId == hobbyId));
+            try {
+                const uhr = user.userHobbies.filter((userHobby) => userHobby.hobbyId == hobbyId)[0];
+                tempHobbyRating = uhr.userRating;
+                setUserHobbyRating(uhr.userRating);
+            } catch {}
+        } catch {
+            history.push("/notfound");
+        }
+    }, [])
 
     async function followHobby(hobbyId) {
         const userId = user.id;
         if (!userId) {
-            setStatus("You have to be logged in to do that.");
+            status = "You have to be logged in to do that.";
             return;
         }
 
@@ -32,36 +41,97 @@ export default function Hobby(props) {
             HobbyId: hobbyId
         };
         try {
-            let response = await axios.post("https://localhost:44394/api/UserHobbyRatings", newUserHobby);
+            let response = await axios.post("https://localhost:44394/api/users/uhr", newUserHobby);
             if (response.data) {
-                setStatus("Successfully subscribed!");
-                refreshUser();
+                status = "Successfully subscribed!";
+                dispatch(setUser(response.data));
+                setUserHobbyRating(0);
             }
         } catch {
-            setStatus("Something went wrong. Please try again");
+            status = "Something went wrong. Please try again.";
         }
     }
 
-    async function refreshUser() {
-        const userId = user.id;
+    async function rateHobby(rating) {
+        const hobbyToUpdate = {
+            "UserId": user.id,
+            "HobbyId": parseInt(hobbyId),
+            "Rating": rating,
+        };
+        console.log(hobbyToUpdate);
         try {
-            let response = await axios.get(`https://localhost:44394/api/users/${userId}`);
+            let response = await axios.put("https://localhost:44394/api/users/uhr", hobbyToUpdate);
             if (response.data) {
+                status = "Sucessfully updated rating!";
                 dispatch(setUser(response.data));
+                setUserHobbyRating(rating);
             }
         } catch {
-            history.push("/notfound");
+            status = "Something went wrong. Please try again.";
         }
+    }
+
+    // async function refreshUser() {
+    //     const userId = user.id;
+    //     try {
+    //         let response = await axios.get(`https://localhost:44394/api/users/${userId}`);
+    //         if (response.data) {
+    //             dispatch(setUser(response.data));
+    //         }
+    //     } catch {
+    //         history.push("/notfound");
+    //     }
+    // }
+
+    const setTempRating = (value) => {
+        tempHobbyRating = value;
     }
 
 
     return (
         <div>
-            <h1>
-                {currentHobby.name}
-            </h1>
-            <button onClick={() => followHobby(hobbyId)} >Follow</button>
-            <h3>{status}</h3>
+            {!currentHobby &&
+                <div>
+                    <h1>Loading...</h1>
+                </div>
+            }
+
+            {currentHobby &&
+                <div>
+                    <h1>
+                        {currentHobby.name}
+                    </h1>
+                    {userHobbyRating !== -1 &&
+                        <Fragment>
+                            <p>Current rating: {userHobbyRating}</p>
+                            <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                                <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" onClick={() => setTempRating(1)} />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio1">1</label>
+
+                                <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" onClick={() => setTempRating(2)} />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio2">2</label>
+
+                                <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" onClick={() => setTempRating(3)} />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio3">3</label>
+
+                                <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off" onClick={() => setTempRating(4)} />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio4">4</label>
+
+                                <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off" onClick={() => setTempRating(5)} />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio5">5</label>
+                            </div>
+                            <button className="btn btn-primary" onClick={() => rateHobby(tempHobbyRating)} >Rate</button>
+                            <h3>{status}</h3>
+                        </Fragment>
+                    }
+                    {userHobbyRating === -1 &&
+                        <Fragment>
+                            <button onClick={() => followHobby(hobbyId)} >Follow</button>
+                            <h3>{status}</h3>
+                        </Fragment>
+                    }
+                </div>
+            }
         </div>
     )
 }
