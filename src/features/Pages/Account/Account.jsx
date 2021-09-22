28d2cdc6e83f6f48d4c27c6af3624a4b84e75cbb
Ryan <ryan@ryannd.com>
2021-09-22 15:38:47 -0500

    Added a way to connect with other users and display a user's connections on the account page

--- src/features/Pages/Account/Account.jsx
+++ src/features/Pages/Account/Account.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 export default function Account(props) {
@@ -10,7 +11,52 @@ export default function Account(props) {
         <div>
             {user &&
                 <div>
+                    <h1>Hello, {user.firstName}!</h1>
                     <h1>{user.userName}</h1>
+
+                    <div>
+                        <h2>Upcoming events:</h2>
+                        {user.attendingEvents.length === 0 && 
+                            <p>No events scheduled yet. Find something to do <Link to="/events" >here!</Link></p>
+                        }
+                        {user.attendingEvents.length > 0 &&
+                            <ol className="list-group list-group-numbered">
+                                {user.attendingEvents.map((attending) => {
+                                    return (
+                                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                                            <Link to={`/events/${attending.username}`} className="ms-2 me-auto">
+                                                <div className="fw-bold">{attending.name}</div>
+                                                <div>{attending.description}</div>
+                                                <div>Location: {attending.location}</div>
+                                                <div>Date: {attending.date}</div>
+                                            </Link>
+                                        </li>
+                                    )
+                                })}
+                            </ol>
+                        }
+                    </div>
+
+                    <div>
+                        <h2>See what your friends are up to</h2>
+                        {user.connections.length === 0 &&
+                            <p>You haven't added any friends yet. Find someone <Link to="/people">here,</Link> or invite someone new!</p>
+                        }
+                        {user.connections.length > 0 &&
+                            <ol className="list-group list-group-numbered">
+                                {user.connections.map((connection) => {
+                                    return (
+                                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                                            <Link to={`/people/${connection.username}`} className="ms-2 me-auto">
+                                                <div className="fw-bold">{connection.username}</div>
+                                                <div>{connection.name}</div>
+                                            </Link>
+                                        </li>
+                                    )
+                                })}
+                            </ol>
+                        }
+                    </div>
                 </div>
             }
             {!user &&
--- src/features/Pages/OtherUser/OtherUser.jsx
+++ src/features/Pages/OtherUser/OtherUser.jsx
@@ -1,16 +1,49 @@
 import axios from 'axios';
 import React, { Fragment, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import useForm from '../../Hooks/useForm';
 
 
 export default function OtherUser(props) {
     const username = props.match.params.username;
     const [user, setUser] = useState();
+    const [statusMessage, setStatusMessage] = useState("");
+    const loggedInUser = useSelector((state) => state.user.value.payload);
+    const { formValues, handleChange, handleSubmit } = useForm(() => connect(user, loggedInUser, formValues));
+
     
     async function getUser() {
         let response = await axios.get(`https://localhost:44394/api/users/view/${username}`);
         if (response.data) {
             setUser(response.data);
+        }
+    }
+
+    async function connect(user, loggedInUser, formValues) {
+        if (loggedInUser) {
+            const now = new Date();
+            const newConnectionInvite = {
+                fromUserId: loggedInUser.id,
+                toUserId: user.id,
+                message: formValues.message,
+                accepted: "false",
+                dateSent: now.getTime() * 10000,
+                dismissed: "false"
+            }
+            console.log(newConnectionInvite);
+            try {
+                let response = await axios.post("https://localhost:44394/api/connectioninvites", newConnectionInvite);
+                console.log(response);
+                if (response.data) {
+                    setStatusMessage("Invite sent!");
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        } else {
+            setStatusMessage("You need to be logged in to do that.");
         }
     }
 
@@ -27,18 +60,34 @@ export default function OtherUser(props) {
                 <Fragment>
                     <h1>{user.username}</h1>
                     <h2>{user.name}</h2>
-                    <h2>Hobbies this person follows:</h2>
-                    <ol className="list-group list-group-numbered">
-                        {user.hobbies.map((hobby) => {
-                            return (
-                                <li className="list-group-item d-flex justify-content-between align-items-start">
-                                    <Link to={`/hobbies/${hobby.hobbyId}`} className="ms-2 me-auto">
-                                        <div className="fw-bold">{hobby.name}</div>
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                    </ol>
+                    
+                    <div>
+                        <h2>Like what you see? Invite them to connect!</h2>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <label for="message" className="form-label">Send them a message! (optional)</label>
+                                <textarea className="form-control" name="message" id="message" rows="3" onChange={handleChange}></textarea>
+                            </div>
+                            <div className="col-12">
+                                <button className="btn btn-primary" type="submit">Get Connected</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div>
+                        <h2>Hobbies this person follows:</h2>
+                        <ol className="list-group list-group-numbered">
+                            {user.hobbies.map((hobby) => {
+                                return (
+                                    <li className="list-group-item d-flex justify-content-between align-items-start">
+                                        <Link to={`/hobbies/${hobby.hobbyId}`} className="ms-2 me-auto">
+                                            <div className="fw-bold">{hobby.name}</div>
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ol>
+                    </div>
                 </Fragment>
             }
         </div>
