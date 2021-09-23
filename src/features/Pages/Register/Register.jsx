a4332843e44d993c8ad47eb3d1a47d4c15ee1297
Ryan <ryan@ryannd.com>
2021-09-23 17:15:24 -0500

    Added contact preference to Registration page

--- src/features/Pages/Register/Register.jsx
+++ src/features/Pages/Register/Register.jsx
@@ -18,7 +18,7 @@ export default function Register(props) {
         phone: true,
         userCity: true,
         userState: true,
-        userZip: true
+        userZip: true,
     });
 
     const validateAndSubmit = (event) => {
@@ -217,6 +217,14 @@ export default function Register(props) {
                 }
                 </div>
             </div>
+            <div className="col-md-3">
+                <label htmlFor="contactPreference" className="form-label">Contact Preference</label>
+                <select className="form-select" name="contactPreference" id="contactPreference" onChange={handleChange} required>
+                    <option value="none" defaultValue >None</option>
+                    <option value="text" >Text Messages</option>
+                    <option value="email" >Email</option>
+                </select>
+            </div>
             <div className="col-md-6">
                 <label htmlFor="userCity" className="form-label">City</label>
                 <input type="text" className="form-control" name="userCity" id="userCity" onChange={validateChange} required />
@@ -230,56 +238,56 @@ export default function Register(props) {
                 <label htmlFor="userState" className="form-label">State</label>
                 <select className="form-select" name="userState" id="userState" onChange={validateChange} required>
                     <option defaultValue value="">Choose...</option>
-                    <option value="AK" onClick={validateChange} >AK</option>
-                    <option value="AL" onClick={validateChange} >AL</option>
-                    <option value="AZ" onClick={validateChange} >AZ</option>
-                    <option value="AR" onClick={validateChange} >AR</option>
-                    <option value="CA" onClick={validateChange} >CA</option>
-                    <option value="CO" onClick={validateChange} >CO</option>
-                    <option value="CT" onClick={validateChange} >CT</option>
-                    <option value="DE" onClick={validateChange} >DE</option>
-                    <option value="FL" onClick={validateChange} >FL</option>
-                    <option value="GA" onClick={validateChange} >GA</option>
-                    <option value="HI" onClick={validateChange} >HI</option>
-                    <option value="ID" onClick={validateChange} >ID</option>
-                    <option value="IL" onClick={validateChange} >IL</option>
-                    <option value="IN" onClick={validateChange} >IN</option>
-                    <option value="IA" onClick={validateChange} >IA</option>
-                    <option value="KS" onClick={validateChange} >KS</option>
-                    <option value="KY" onClick={validateChange} >KY</option>
-                    <option value="LA" onClick={validateChange} >LA</option>
-                    <option value="ME" onClick={validateChange} >ME</option>
-                    <option value="MD" onClick={validateChange} >MD</option>
-                    <option value="MA" onClick={validateChange} >MA</option>
-                    <option value="MI" onClick={validateChange} >MI</option>
-                    <option value="MN" onClick={validateChange} >MN</option>
-                    <option value="MS" onClick={validateChange} >MS</option>
-                    <option value="MO" onClick={validateChange} >MO</option>
-                    <option value="MT" onClick={validateChange} >MT</option>
-                    <option value="NE" onClick={validateChange} >NE</option>
-                    <option value="NV" onClick={validateChange} >NV</option>
-                    <option value="NH" onClick={validateChange} >NH</option>
-                    <option value="NJ" onClick={validateChange} >NJ</option>
-                    <option value="NM" onClick={validateChange} >NM</option>
-                    <option value="NY" onClick={validateChange} >NY</option>
-                    <option value="NC" onClick={validateChange} >NC</option>
-                    <option value="ND" onClick={validateChange} >ND</option>
-                    <option value="OH" onClick={validateChange} >OH</option>
-                    <option value="OK" onClick={validateChange} >OK</option>
-                    <option value="OR" onClick={validateChange} >OR</option>
-                    <option value="PA" onClick={validateChange} >PA</option>
-                    <option value="RI" onClick={validateChange} >RI</option>
-                    <option value="SC" onClick={validateChange} >SC</option>
-                    <option value="SD" onClick={validateChange} >SD</option>
-                    <option value="TN" onClick={validateChange} >TN</option>
-                    <option value="TX" onClick={validateChange} >TX</option>
-                    <option value="UT" onClick={validateChange} >UT</option>
-                    <option value="VT" onClick={validateChange} >VT</option>
-                    <option value="VA" onClick={validateChange} >VA</option>
-                    <option value="WA" onClick={validateChange} >WA</option>
-                    <option value="WV" onClick={validateChange} >WV</option>
-                    <option value="WI" onClick={validateChange} >WI</option>
-                    <option value="WY" onClick={validateChange} >WY</option>
+                    <option value="AK" >AK</option>
+                    <option value="AL" >AL</option>
+                    <option value="AZ" >AZ</option>
+                    <option value="AR" >AR</option>
+                    <option value="CA" >CA</option>
+                    <option value="CO" >CO</option>
+                    <option value="CT" >CT</option>
+                    <option value="DE" >DE</option>
+                    <option value="FL" >FL</option>
+                    <option value="GA" >GA</option>
+                    <option value="HI" >HI</option>
+                    <option value="ID" >ID</option>
+                    <option value="IL" >IL</option>
+                    <option value="IN" >IN</option>
+                    <option value="IA" >IA</option>
+                    <option value="KS" >KS</option>
+                    <option value="KY" >KY</option>
+                    <option value="LA" >LA</option>
+                    <option value="ME" >ME</option>
+                    <option value="MD" >MD</option>
+                    <option value="MA" >MA</option>
+                    <option value="MI" >MI</option>
+                    <option value="MN" >MN</option>
+                    <option value="MS" >MS</option>
+                    <option value="MO" >MO</option>
+                    <option value="MT" >MT</option>
+                    <option value="NE" >NE</option>
+                    <option value="NV" >NV</option>
+                    <option value="NH" >NH</option>
+                    <option value="NJ" >NJ</option>
+                    <option value="NM" >NM</option>
+                    <option value="NY" >NY</option>
+                    <option value="NC" >NC</option>
+                    <option value="ND" >ND</option>
+                    <option value="OH" >OH</option>
+                    <option value="OK" >OK</option>
+                    <option value="OR" >OR</option>
+                    <option value="PA" >PA</option>
+                    <option value="RI" >RI</option>
+                    <option value="SC" >SC</option>
+                    <option value="SD" >SD</option>
+                    <option value="TN" >TN</option>
+                    <option value="TX" >TX</option>
+                    <option value="UT" >UT</option>
+                    <option value="VT" >VT</option>
+                    <option value="VA" >VA</option>
+                    <option value="WA" >WA</option>
+                    <option value="WV" >WV</option>
+                    <option value="WI" >WI</option>
+                    <option value="WY" >WY</option>
                 </select>
                 {!valid.userState && 
                     <div>
